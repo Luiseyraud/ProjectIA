e70a1a68d3d39913dea0d332586e2550dc92c0c9
noussnouss32 <80573795+noussnouss32@users.noreply.github.com>
2026-03-27 11:29:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -44,13 +44,16 @@ function dessinerJeu() {
     // 1. On efface tout le canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // 2. On dessine la carte (les murs)
-    if (mapData) {
-        ctx.fillStyle = "#2c3e50"; // Couleur des murs (gris foncé)
+    // 2. On dessine la carte (les murs et les safes places)
+   if (mapData) {
         for (let y = 0; y < mapH; y++) {
             for (let x = 0; x < mapW; x++) {
                 let index = y * mapW + x;
                 if (mapData[index] === 1) { // Si c'est un mur
+                    ctx.fillStyle = "#2c3e50"; // Couleur des murs (gris foncé)
+                    ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+                }else if (mapData[index] === 2){
+                    ctx.fillStyle = "#ab3954"; // Couleur des murs (gris foncé)
                     ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
                 }
             }
@@ -62,6 +65,34 @@ function dessinerJeu() {
     ctx.beginPath();
     ctx.arc(bot.x, bot.y, 10, 0, Math.PI * 2);
     ctx.fill();
+
+    
+    // Variable pour le clignotement (compteur d'images)
+    let frameCount = 0;
+
+    // LOGIQUE DU BOT QUI PANIQUE
+    if (etatPaniqueDuBot === 1) {
+        // Le bot panique. 
+        // frameCount % 20 < 10 signifie: rouge pendant 10 images, puis couleur normale pendant 10 images.
+        if (frameCount % 20 < 10) {
+            ctx.fillStyle = "red"; // Rouge vif
+        
+        } else {
+            ctx.fillStyle = "#f1c40f"; // Couleur normale du bot (rouge plus doux)
+        }
+    } else {
+        // Le bot est calme
+        ctx.fillStyle = "#f1c40f"; // Couleur normale
+    }
+
+    ctx.fill();
+    ctx.closePath();
+
+    // Incrémenter le compteur d'images
+    frameCount++;
+    
+    // Demander l'image suivante
+    requestAnimationFrame(dessinerJeu);
 
     // 4. On dessine le Joueur (le requin)
     ctx.fillStyle = "#e74c3c"; // Rouge
